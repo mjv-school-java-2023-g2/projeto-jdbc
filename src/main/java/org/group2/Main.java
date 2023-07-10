44bfd3c992353847ceafe494ffba649f872acfde
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-07-09 21:52:59 -0300

    refatorando

--- src/main/java/org/group2/Main.java
+++ src/main/java/org/group2/Main.java
@@ -2,6 +2,7 @@ package org.group2;
 
 import org.group2.core.model.Professor;
 import org.group2.core.repository.ProfessorRepository;
+import org.group2.infra.ConnectionFactory;
 import org.group2.infra.PostgresCrud;
 
 import java.math.BigDecimal;
@@ -11,6 +12,8 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
+
+        ConnectionFactory.startConnection();
 
         ProfessorRepository professorRepository = new PostgresCrud();
 
@@ -27,5 +30,19 @@ public class Main {
         );
 
         professorRepository.save(professor);
+
+        System.out.println(professorRepository.getById(1L));
+
+        professorRepository.getAll().forEach(System.out::println);
+
+        professor.setNome("Maria Maria");
+
+        professorRepository.update(professor, 1L);
+
+        System.out.println(professorRepository.getById(1L));
+
+        professorRepository.delete(1L);
+
+        System.out.println(professorRepository.getById(1L));
     }
 }
--- src/main/java/org/group2/infra/ConnectionFactory.java
+++ src/main/java/org/group2/infra/ConnectionFactory.java
@@ -1,26 +1,33 @@
 package org.group2.infra;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
     private static Connection connection;
-    public static void startConnection(){
-        try{
-            if(connection==null) {
+
+    public static void startConnection() {
+        try {
+            if (connection == null) {
                 connection = DriverManager.getConnection(
-                        "jdbc:postgresql://localhost:5432/live-spring-data-jpa",
-                        "postgres",
-                        "1234567"
+                        "jdbc:postgresql://" +
+                                System.getenv("DB_HOST") + ":" +
+                                System.getenv("DB_PORT") + "/" +
+                                System.getenv("DB_DATABASE"),
+                                System.getenv("DB_USERNAME"),
+                                System.getenv("DB_PASSWORD")
                 );
                 System.out.println("CONEXAO REALIZADA COM SUCESSO");
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             throw new RuntimeException("Erro ao tentar realizar uma conexão");
         }
     }
-    public static Connection getConnection(){
+
+    public static Connection getConnection() {
 
         return connection;
     }
